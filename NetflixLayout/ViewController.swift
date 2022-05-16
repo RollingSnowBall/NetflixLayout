83c0fcffaf69f6a8c77fc1f45108bbdeacf6580c
JUNO <jrlee0701@kakao.com>
2022-05-16 17:59:50 +0900

    Initial Commit

---
+++ NetflixLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixLayout
+//
+//  Created by JUNO on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
